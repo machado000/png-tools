7f2fc635c5587dafd01c0cfc9a00e91ac414d87b
Joao Machado <machado000@gmail.com>
2025-08-20 19:06:05 -0300

    Review optimize code

--- build_gif_from_images.py
+++ build_gif_from_images.py
@@ -1,13 +1,13 @@
 import moviepy as mp
 import os
-
+import subprocess
 from pathlib import Path
 
 
 project_path = Path(__file__).parent.resolve()
-source_path = "I:\\Meu Drive\\02 - MATERIAIS\\024_NACIONAL RIO_CMRJ_CAMERA APROVA\\nacional_cmrj_cidadania_v5"
-destination_path = "I:\\Meu Drive\\02 - MATERIAIS\\024_NACIONAL RIO_CMRJ_CAMERA APROVA\\nacional_cmrj_cidadania_gifs_2"
-# gifsicle_path = os.path.join(project_path, "gifsicle", "gifsicle.exe")
+source_path = "I:\\Meu Drive\\02 - MATERIAIS\\034_APEX_BONIFICADO\\nova_apex_web_summit_lisboa"
+destination_path = "I:\\Meu Drive\\02 - MATERIAIS\\034_APEX_BONIFICADO\\nova_apex_web_summit_lisboa"
+gifsicle_path = os.path.join(project_path, "gifsicle", "gifsicle.exe")
 
 os.makedirs(destination_path, exist_ok=True)
 
@@ -19,9 +19,10 @@ for subfolder in subfolders:
     output_path = os.path.join(destination_path, f"{subfolder}.gif")
 
     # Config
-    image_files = [os.path.join(subfolder_path, f"{i:02}.png") for i in range(1, 10)]
-    timestamps = [0, 0.3, 0.6, 4, 4.3, 4.6, 9, 9.3, 9.6]  # when each image appears (in seconds)
-    duration = 12
+    image_files = [os.path.join(subfolder_path, f"{i:03}.png") for i in range(1, 16)]
+    timestamps = [0, 0.2, 0.4, 0.6, 4, 4.2, 4.4, 4.6, 4.8, 5, 12,
+                  12.2, 12.4, 12.6, 12.8]  # when each image appears (in seconds)
+    duration = 16
 
     # Create base empty clip
     clips = []
@@ -32,14 +33,14 @@ for subfolder in subfolders:
     final = mp.CompositeVideoClip(clips, size=clips[0].size).with_duration(duration)
     final.write_gif(output_path, fps=10)
 
-    # # Optimize GIF with pygifsicle
-    # optimized_gif_path = os.path.join(destination_path, f"{subfolder}_otimized.gif")
-    # subprocess.run([
-    #     gifsicle_path,
-    #     "-b",
-    #     "--optimize=3",
-    #     "--colors=256",
-    #     "--lossy=100",
-    #     gif_path,
-    #     "-o", optimized_gif_path,
-    # ], check=True)
+    # Optimize GIF with pygifsicle
+    optimized_gif_path = os.path.join(destination_path, f"{subfolder}_otimized.gif")
+    subprocess.run([
+        gifsicle_path,
+        "-b",
+        "--optimize=2",
+        "--colors=256",
+        "--lossy=20",
+        output_path,
+        "-o", optimized_gif_path,
+    ], check=True)
